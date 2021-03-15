0d92ed09ef1879f0f9bb19146f576007282d943d
Crypt0p <ayush.deep99@gmail.com>
2021-03-15 10:54:31 +0530

    Solved Problem 2

--- problem2_assignment.c
+++ problem2_assignment.c
@@ -1,139 +1,233 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int SIZE =  100;
+const int INF = 1e6 + 5;
 
 struct Process
 {
-    int processno;
-    int AT;                     // Arrival Time
-    int BT;                     // Burst Time
-    int P;                      // Priority
-    int WT;                     // Wait Time
-    int CT;                     // Completion Time
+    int id;
+    int AT;
+    int BT;
+    int PRT;
+    int CT;
 };
 
-struct Process ReadyQueue[SIZE];
+int n;
+float w1, w2;
+struct Process processData[1000];
+int ReadyQueue[1000];
+int sequence[1000];
+int burst[1000];
 
-int queueFilled = 0; //How much the Queue is FILLED
-
-int Running; //Stores value of Running Proecess
-
- //Returns 1 if we should Run process p1, Returns 0 if we should run p2
- 
-int compareProcess(struct Process p1, struct Process p2, float W1, float W2)
+int min(int i1, int i2)
 {
-    float ans = (W1 * (p1.BT - p2.BT)) + (W2 * (p1.P - p2.P));
-    if (ans < 0 || (ans == 0 && p1.AT > p2.AT))
+    if (i1 <= i2)
     {
-        return 0;
+        return i1;
     }
     else
-        return 1;
+        return i2;
 }
 
-
-void main()
+double max(double d1, double d2)
 {
-   // Running = NULLPROCESS;
-#ifndef ONLINE_JUDGE
-
-    //freopen("input.txt", "r", stdin);
-
-    //freopen("output.txt", "w", stdout);
-
-#endif
-    int n;
-    float w1, w2;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-    struct Process process[n];
-    for (int i = 0; i < n; i++)
+    if (d1 >= d2)
     {
-        char temp;
-        process[i].processno = i + '1';
-        printf("\nEnter arrival time of P%d: ", i + 1);
-        scanf("%d", &process[i].AT);
-        printf("Enter CPU burst cycle of P%d: ", i + 1);
-        scanf("%d", &process[i].BT);
-        printf("Enter prioroity of P%d: ", i + 1);
-        scanf("%d", &process[i].P);
-        process[i].CT = -1;
+        return d1;
     }
-    printf("\nEnter value of w1: ");
-    scanf("%f", &w1);
-    printf("Enter value of w2: ");
-    scanf("%f", &w2);
+    else
+        return d2;
+}
 
-    int processCompleted = 0;
-    int t = 0;
-    char sequence[100];
-    int seq = 0;
-    char lastProc = 'n';
-
-    //SORT PROCESS BY ARRIVAL TIME(O(n^2))
-    for (int i = 0; i < n; i++)
+int fillReadyQueue(int T)
+{
+    //printf("Entered readyqueue\n");
+    int fillCount = 0;
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n - 1; j++)
+        if (processData[i].AT <= T && processData[i].CT == INF)
         {
-            if (process[j].AT > process[j + 1].AT)
-            {
-                struct Process tempProcess;
-                tempProcess = process[j];
-                process[j] = process[j + 1];
-                process[j + 1] = tempProcess;
-            }
+            ReadyQueue[fillCount++] = processData[i].id;
         }
     }
+    return fillCount;
+}
 
-    int i = 0;
-    while (processCompleted!=n)
+double paramValue(int processID)
+{
+    return w1 * processData[processID - 1].BT + w2 * processData[processID - 1].PRT;
+}
+
+int findBestProcess(int RQSize)
+{
+    double mxParam = 0.0;
+    int minArrival = INF;
+    for (int i = 0; i < RQSize; ++i)
     {
-        while(process[i].AT==t){
-            ReadyQueue[queueFilled++] = process[i++];
-        }
-        int nxtArrivalTime = process[i].AT;
-        struct Process mxproc = ReadyQueue[0];
-
-        for(int j=1;j<queueFilled;++j){
-            if(!compareProcess(mxproc,process[j],w1,w2)){
-                mxproc = process[j]; 
-            }
-        }
-
-        Running = mxproc.processno;
-
-        int exec_time = min(t+mxproc.BT,nxtArrivalTime);
-        if(exec_time==t+mxproc.BT){
-            processCompleted++;
-            process[Running].CT = t+exec_time;
-            i++;
-        }
-        mxproc.BT-=exec_time;
-        t+=exec_time;
+        mxParam = max(mxParam, paramValue(ReadyQueue[i]));
     }
-    
+    for (int i = 0; i < RQSize; ++i)
+    {
+        if (paramValue(ReadyQueue[i]) == mxParam)
+        {
+            minArrival = min(minArrival, processData[ReadyQueue[i] - 1].AT);
+        }
+    }
+    for (int i = 0; i < RQSize; ++i)
+    {
+        if (paramValue(ReadyQueue[i]) == mxParam && processData[ReadyQueue[i] - 1].AT == minArrival)
+        {
+            return ReadyQueue[i];
+        }
+    }
+}
 
-    float sum;
+int nxtArrival(int T)
+{
+    // printf("You passed a time of : %d\n", T);
+    int nxat = INF;
+    for (int i = 0; i < n; ++i)
+    {
+        if (processData[i].AT > T)
+        {
+            nxat = min(nxat, processData[i].AT);
+        }
+    }
+    return nxat;
+}
+
+void waitTime()
+{
+    float sum = 0.0;
     for (int i = 0; i < n; i++)
     {
-        int wt = process[i].CT - process[i].AT - process[i].BT;
+        int wt = processData[i].CT - processData[i].AT - burst[i];
         printf("\nWaiting time of P%d: %d", i + 1, wt);
         sum += wt;
     }
     printf("\n");
 
-    printf("\nAverage waiting time: %f", sum / (float)n);
+    printf("\nAverage waiting time: %.4f", sum / (float)n);
     printf("\n");
+}
 
-    printf("\nProcess Execution Order: ");
-    for (int i = 0; i < seq; i++)
+void execOrder()
+{
+    int i = 0;
+    while (sequence[i] != 0)
     {
-        printf("%c", sequence[i]);
+        if (sequence[i] != sequence[i + 1])
+        {
+            if (sequence[i + 1] == 0)
+                printf("P%d", sequence[i]);
+            else
+            {
+                if (sequence[i] == -1)
+                    printf("IDLE->");
+                else printf("P%d->",sequence[i]);
+            }
+        }
+        i++;
     }
     printf("\n");
+}
+
+void completeTime()
+{
+    printf("\n");
+    for (int i = 0; i < n; ++i)
+    {
+        printf("P%d finished at: %d\n", i + 1, processData[i].CT);
+    }
+}
+
+int main()
+{
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
     for (int i = 0; i < n; i++)
     {
-        printf("\nP%d finished at: %d\n", i + 1, process[i].CT);
+        processData[i].id = i + 1;
+        printf("\nEnter arrival time of P%d: ", i + 1);
+        scanf("%d", &processData[i].AT);
+        printf("Enter CPU burst cycle of P%d: ", i + 1);
+        scanf("%d", &processData[i].BT);
+        printf("Enter prioroity of P%d: ", i + 1);
+        scanf("%d", &processData[i].PRT);
+        processData[i].CT = INF;
+        burst[i] = processData[i].BT;
     }
+
+    printf("\nEnter value of w1: ");
+    scanf("%f", &w1);
+    printf("Enter value of w2: ");
+    scanf("%f", &w2);
+
+    int idx = 0, completedProcesses = 0, curTime = 0;
+
+    while (completedProcesses != 3)
+    {
+        int readyQueueFillSize = fillReadyQueue(curTime);
+
+        if (readyQueueFillSize == 0)
+            break;
+
+        // for (int i = 0; i < readyQueueFillSize; ++i)
+        // {
+        //     printf("\nCurrently in Ready Queue : P%d\n", ReadyQueue[i]);
+        // }
+
+        // printf("Ready Queue Size : %d\n", readyQueueFillSize);
+
+        if (readyQueueFillSize == 0)
+        {
+            sequence[idx++] = -1;
+            curTime++;
+            continue;
+        }
+        int RunningID = findBestProcess(readyQueueFillSize);
+
+        // printf("Currently Best Process : P%d\n", RunningID);
+
+        sequence[idx++] = RunningID;
+
+        int nxtTime = nxtArrival(curTime);
+
+        // printf("Next Nearest Process would arrive at : %d\n", nxtTime);
+
+        int execTime;
+
+        if (nxtTime == INF)
+        {
+            execTime = processData[RunningID - 1].BT;
+        }
+        else
+            execTime = min(nxtTime - curTime, processData[RunningID - 1].BT);
+
+        // printf("This process executed for : %d\n", execTime);
+
+        // printf("This process has left burst of : %d\n", processData[RunningID - 1].BT);
+
+        processData[RunningID - 1].BT -= execTime;
+
+        // printf("This process has left burst of : %d\n", processData[RunningID - 1].BT);
+
+        curTime += execTime;
+
+        //printf("The time now is : %d\n",curTime);
+
+        if (processData[RunningID - 1].BT == 0)
+        {
+            completedProcesses++;
+            processData[RunningID - 1].CT = curTime;
+        }
+    }
+
+    waitTime();
+
+    execOrder();
+
+    completeTime();
 }
